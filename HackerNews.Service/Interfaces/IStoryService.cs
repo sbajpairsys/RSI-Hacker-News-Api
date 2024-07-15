731f5cd7199c18b10739028bccd8dbeba2235aa5
sbajpairsys <65710033+sbajpairsys@users.noreply.github.com>
2024-07-15 15:22:41 +0530

    Add files via upload

---
+++ HackerNews.Service/Interfaces/IStoryService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HackerNews.Data.Models;
+
+namespace HackerNews.Business.Interfaces
+{
+
+    /// <summary>
+    /// Defines the <see cref="IStoryService" />.
+    /// </summary>
+    public interface IStoryService
+    {
+        /// <summary>
+        /// Get top story details.
+        /// </summary>
+        /// <returns>List<Story></returns>
+        Task<IEnumerable<Story>> GetTopStoriesAsync();
+
+        Task<Story> GetStoryByIdAsync(int id);
+
+
+    }
+}
