31c0aba794511775fa4fd8dbaeef65817a0182b6
sbajpairsys <65710033+sbajpairsys@users.noreply.github.com>
2024-07-15 15:46:43 +0530

    Add files via upload

---
+++ HackerNews.Service/Program.cs
@@ -0,0 +1,22 @@
+using System.Diagnostics.CodeAnalysis;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+  
+}
+
+app.UseHttpsRedirection();
+app.Run();
+
+[ExcludeFromCodeCoverage]
+public partial class Program
+{
+}
